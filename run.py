5dbc264a029390f66cd152b86df780ef6b8764d7
TabooNeter12 <46275052+TabooNeter@users.noreply.github.com>
2020-01-24 16:15:56 +0400

    Add files via upload

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
-#from flaskblog import app
+from flaskblog import app
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
+
+#from flask import Flask
+#app = Flask(__name__)
