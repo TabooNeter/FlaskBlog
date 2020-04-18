03ee6dea7a14a66238685ff3bd91bd53b58f2834
TabooNeter12 <46275052+TabooNeter@users.noreply.github.com>
2020-04-18 07:12:28 +0400

    Add files via upload

---
+++ flaskblog/users/utils.py
@@ -0,0 +1,28 @@
+import os
+import secrets
+from PIL import Image
+from flask import url_for
+from flask_mail import Message
+from flaskblog import app, mail
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page=page, per_page=5)
+    return render_template('user_posts.html', posts=posts, user=user)
